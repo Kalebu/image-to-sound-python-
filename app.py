1c503ca2ffe7dba53144c6d3393933129b2c1c5f
Kalebu Gwalugano <isaackeinstein@gmail.com>
2020-11-05 13:46:49 +0300

    improving code readability

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from PIL import Image
 from gtts import gTTS
 from pytesseract import image_to_string
-clean = lambda text : ' '.join(text.split('\n'))
-to_text = lambda image: clean(image_to_string(Image.open(image)))
-to_sound = lambda text: gTTS(text, lang='en').save('gene.mp3')
-image_to_sound  = lambda image: to_sound(to_text(image))
+
+def image_to_sound(path_to_image):
+
+    try:
+        loaded_image = Image.open(path_to_image)
+
+        decoded_text = image_to_string(loaded_image)
+
+        cleaned_text = ' '.join(decoded_text.split('\n'))
+
+        print(cleaned_text)
+         
+        sound = gTTS(cleaned_text, lang = 'en')
+
+        sound.save('sound.mp3')      
+
+        return True
+
+    except Exception as bug:
+
+        print('The bug thrown while excuting the code\n', bug)
+
+        return
+        
 image_to_sound('image.jpg')
 input()
